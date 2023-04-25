a997a60b0dc4ae157eafd712b939e7021b65077b
Dipesh Singh <sinhdipesh@gmail.com>
2023-04-25 17:56:06 +0530

    Delete UserManagement directory

--- UserManagement/src/main/java/com/usermanagement/repositories/UserDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.usermanagement.repositories;
-
-import com.usermanagement.models.UserDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDetailRepository extends JpaRepository<UserDetail,Integer> {
-}
