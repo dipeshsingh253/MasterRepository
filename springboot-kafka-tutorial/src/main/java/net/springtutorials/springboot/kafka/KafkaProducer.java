94e928df0dc0b7b30ddf145f33d4ba8ff7552bab
Dipesh Singh <sinhdipesh@gmail.com>
2023-05-19 17:58:43 +0530

    Spring boot + kafka
    I have create kafka producer and consumer both in this tutorial.
    I have used confluent kafka to achieve the result.

---
+++ springboot-kafka-tutorial/src/main/java/net/springtutorials/springboot/kafka/KafkaProducer.java
@@ -0,0 +1,32 @@
+package net.springtutorials.springboot.kafka;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+    private KafkaTemplate<String,String> kafkaTemplate;
+
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message){
+        LOGGER.info("Message Sent : {}",message);
+        kafkaTemplate.send("anime",message);
+    }
+
+
+    public void sendMessageToTopic(String topic, String message){
+        LOGGER.info("Message sent to topic : {}",topic);
+        kafkaTemplate.send(topic,message);
+    }
+
+}
